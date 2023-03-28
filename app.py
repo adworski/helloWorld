a3c6946482d6fff588908e4d6fda2eeb2545bed6
Andrew <adworski@terpmail.umd.edu>
2023-03-28 11:44:04 -0400

    added navbar, used jinja formatting, and added new pages

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World from Robert Smith! I am adding my first code change.'
+def base():  # put application's code here
+    return render_template('base.html')
+
 
 @app.route('/hello')
 def hello():  # put application's code here
     return render_template('hello.html')
 
+
 @app.route('/about')
 def about():  # put application's code here
     return render_template('about.html')
 
+
+@app.route('/fav')
+def fav_course():  # put application's code here
+    print('You entered favorite course as: ' + request.args.get('course_name') + request.args.get('course_number'))
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():  # put application's code here
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
