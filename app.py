5f42352ff273e770d4adb4a6c6eab389ef16ac17
Andrew <adworski@terpmail.umd.edu>
2023-04-04 11:21:34 -0400

    trying to fix python anywhere
    part 2

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def student_view(student_id):
         flash(f'Student attempting to be viewed could not be found!', 'error')
         return redirect(url_for('student_view_all'))
 
+
 @app.route('/student/create', methods=['GET', 'POST'])
 def student_create():
     if request.method == 'GET':
@@ -46,11 +47,10 @@ def student_create():
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         major_id = request.form['major_id']
-
+        email = request.form['email']
         birth_date = request.form['birth_date']
         is_honors = True if 'is_honors' in request.form else False
-
-        student = Student(first_name=first_name, last_name=last_name, major_id=major_id,
+        student = Student(first_name=first_name, last_name=last_name, major_id=major_id, email=email,
                           birth_date=dt.strptime(birth_date, '%Y-%m-%d'), is_honors=is_honors)
         db.session.add(student)
         db.session.commit()
@@ -82,6 +82,7 @@ def student_edit(student_id):
             student.first_name = request.form['first_name']
             student.last_name = request.form['last_name']
             student.major_id = request.form['major_id']
+            student.email = request.form['email']
             student.birthdate = dt.strptime(request.form['birth_date'], '%Y-%m-%d')
             student.num_credits_completed = request.form['num_credits_completed']
             student.gpa = request.form['gpa']
@@ -95,6 +96,7 @@ def student_edit(student_id):
         return redirect(url_for('student_view_all'))
 
     return redirect(url_for('student_view_all'))
+
 
 @app.route('/student/delete/<int:student_id>')
 def student_delete(student_id):
@@ -120,7 +122,7 @@ if __name__ == '__main__':
         db.create_all()
 
         # Execute only once! Initial loading of available majors
-        """
+'''
         majors = ['Accounting', 'Finance', 'Information Systems', 'International Business', 'Management', \
                     'Operations Management & Business Analytics', 'Supply Chain Management']
         for each_major in majors:
@@ -128,10 +130,8 @@ if __name__ == '__main__':
             amajor = Major(major=each_major)
             db.session.add(amajor)
             db.session.commit()
-        """
-    app.run()
 
+'''
 
-
-
+app.run()
 
